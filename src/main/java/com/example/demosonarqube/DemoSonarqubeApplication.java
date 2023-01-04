900c560e516231c191c612f88390e650ce70f68e
abc224 <abc.alybadaraconde@gmail.com>
2023-01-04 16:42:36 +0100

    second commit

---
+++ src/main/java/com/example/demosonarqube/DemoSonarqubeApplication.java
@@ -0,0 +1,13 @@
+package com.example.demosonarqube;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSonarqubeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSonarqubeApplication.class, args);
+	}
+
+}
